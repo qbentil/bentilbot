51bfaab81b5b5304b62e7d39b6d9226c83f15d8f
qbentil <Bentilshadrack72@gmail.com>
2022-01-05 22:42:26 +0000

    Initial COmmit

---
+++ README.md
@@ -0,0 +1,14 @@
+# twitterbot
+To use this bot,
+You will require API Key and Access Key.
+
+Signup at https://developer.twitter.com to get access to the twitter API Keys for develpers
+
+##### About the Bot ######
+# bot.py
+This file checks tweets the bot is mentioned in.
+It then like, reply and retweet those tweets.
+
+# searchbar.py
+This file searches for tweets with a given hashtag.
+It then likes retweet those tweets.
---
+++ searchbar.py
@@ -0,0 +1,41 @@
+import tweepy
+import time 
+
+apikey = ""
+apikey_sec = ""
+
+access = ""
+access_sec = ""
+
+
+auth = tweepy.OAuthHandler(apikey, apikey_sec)
+auth.set_access_token(access, access_sec)
+
+
+# Seconde
+api = tweepy.API(auth)
+# api.update_status('Bentilbot reporting in live!')
+
+
+# hashtag = "#CodeNewbie"
+tweetHandler = 10
+
+
+
+def searchBot(HASH_TAG):
+    tweets = tweepy.Cursor(api.search, HASH_TAG).items(tweetHandler)
+    for tweet in tweets:
+        try:
+            tweet.retweet()
+            api.create_favorite(tweet.id)
+            print("Retweet Done")
+            time.sleep(2)
+        except tweepy.TweepError as e:
+            print(e.reason)
+            time.sleep(2)
+
+            
+# Run the function with the hashtag you want to search as parameter
+searchBot("#100DaysOfCode")
+searchBot("#themanbentil")
+searchBot("#pukkaTech")
